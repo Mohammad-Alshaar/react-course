2c96631cdfc185a911712790d7d4b949e7ba72b0
Moshaar <moalshaar225@gmail.com>
2024-08-14 13:29:16 +0300

    add some edits

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Muhammad Al-Shaar</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <>
+      <header>Welcome</header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">about</a>
+          </li>
+          <li>
+            <a href="#">X</a>
+          </li>
+          <li>
+            <a href="#">Y</a>
+          </li>
+          <li>
+            <a href="#">Z</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
